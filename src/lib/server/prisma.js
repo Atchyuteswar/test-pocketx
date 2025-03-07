dd0500f7a4833eed157540826aeb7f7b62e188ef
Atchyuteswar <yt.atchyut@gmail.com>
2025-03-07 09:14:33 +0530

    ...

--- src/lib/server/prisma.js
+++ src/lib/server/prisma.js
@@ -1,8 +1,9 @@
-// src/lib/server/prisma.js
 import { PrismaClient } from '@prisma/client';
 
+// Create a singleton instance of Prisma Client
 const prisma = global.__prisma || new PrismaClient();
 
+// In development, keep the connection between reloads
 if (process.env.NODE_ENV === 'development') {
   global.__prisma = prisma;
 }
